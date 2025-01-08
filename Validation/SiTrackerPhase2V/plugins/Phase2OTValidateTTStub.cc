c842cb46b5e98c1409842a93f58e94209a2fe8df
Brandi Skipworth <bskipwo1@vols.utk.edu>
2025-01-08 15:49:20 +0100

    changed binning for PS modules

--- Validation/SiTrackerPhase2V/plugins/Phase2OTValidateTTStub.cc
+++ Validation/SiTrackerPhase2V/plugins/Phase2OTValidateTTStub.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*-
+ -*- C++ -*-
 //
 /**\class SiOuterTracker Phase2OTValidateTTStub.cc
  Description: [one line class summary]
@@ -111,7 +111,9 @@ private:
   const TrackerTopology* tTopo_ = nullptr;
   double TP_minPt;
   double TP_maxEta;
-  float TP_maxD0;
+  double TP_maxVtxZ;
+  double TP_maxD0;
+  double TP_maxDxy;
 };
 
 // constructors and destructor
@@ -127,7 +129,9 @@ Phase2OTValidateTTStub::Phase2OTValidateTTStub(const edm::ParameterSet& iConfig)
       consumes<TTStubAssociationMap<Ref_Phase2TrackerDigi_>>(conf_.getParameter<edm::InputTag>("MCTruthStubInputTag"));
   TP_minPt = conf_.getParameter<double>("TP_minPt");
   TP_maxEta = conf_.getParameter<double>("TP_maxEta");
+  TP_maxVtxZ = conf_.getParameter<double>("TP_maxVtxZ");
   TP_maxD0 = conf_.getParameter<double>("TP_maxD0");
+  TP_maxDxy = conf_.getParameter<double>("TP_maxDxy");
 }
 
 Phase2OTValidateTTStub::~Phase2OTValidateTTStub() {
@@ -277,7 +281,7 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
       Stub_RZ->Fill(posStub.z(), posStub.perp());
     }
   }
-
+  
   // Loop over geometric detectors
   for (auto gd = tkGeom_->dets().begin(); gd != tkGeom_->dets().end(); gd++) {
     DetId detid = (*gd)->geographicalId();
@@ -362,6 +366,7 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
       }
 
       int isPSmodule = (topo.nrows() == 960) ? 1 : 0;
+
       // Calculate local coordinates of clusters
       MeasurementPoint innerClusterCoords = tempStubPtr->clusterRef(0)->findAverageLocalCoordinatesCentered();
       MeasurementPoint outerClusterCoords = tempStubPtr->clusterRef(1)->findAverageLocalCoordinatesCentered();
@@ -385,6 +390,10 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
       float tp_pt = associatedTP->p4().pt();
       float tp_eta = associatedTP->p4().eta();
       float tp_d0 = associatedTP->d0();
+      float tp_vx = associatedTP->vx();
+      float tp_vy = associatedTP->vy();
+      float tp_vz = associatedTP->vz();
+      float tp_dxy = std::sqrt(tp_vx * tp_vx + tp_vy * tp_vy);
 
       if (tp_charge == 0)
         continue;
@@ -392,7 +401,11 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
         continue;
       if (std::abs(tp_eta) > TP_maxEta)
         continue;
+      if (std::abs(tp_vz) > TP_maxVtxZ)
+        continue;
       if (std::abs(tp_d0) > TP_maxD0)
+        continue;
+      if (std::abs(tp_dxy) > TP_maxDxy)
         continue;
 
       // Derived coordinates
@@ -400,10 +413,6 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
       float tp_z = tpDerivedCoords[0];
       float tp_phi = tpDerivedCoords[1];
       float tp_r = tpDerivedCoords[2];
-
-      //if (tp_r > 120 || tp_r < 20) {
-      //std::cout << " stub_r: " << stub_r << " tp_r: " << tp_r << std::endl;
-      //}
 
       // Trigger information
       float trigBend = tempStubPtr->bendFE();
@@ -470,7 +479,7 @@ void Phase2OTValidateTTStub::analyze(const edm::Event& iEvent, const edm::EventS
         (*phi_res_vec)[layer - 1]->Fill(phiRes);
       }
     }
-  }
+  } 
 }  // end of method
 
 // ------------ method called when starting to processes a run  ------------
@@ -478,8 +487,8 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
                                             edm::Run const& run,
                                             edm::EventSetup const& es) {
   edm::ParameterSet psTTStub_RZ = conf_.getParameter<edm::ParameterSet>("TH2TTStub_RZ");
-  edm::ParameterSet psZ_Res = conf_.getParameter<edm::ParameterSet>("TH1z_Res");
-  edm::ParameterSet psR_Res = conf_.getParameter<edm::ParameterSet>("TH1r_Res");
+  edm::ParameterSet ps_2S_Res = conf_.getParameter<edm::ParameterSet>("TH1_2S_Res");
+  edm::ParameterSet ps_PS_Res = conf_.getParameter<edm::ParameterSet>("TH1_PS_Res");
   edm::ParameterSet psPhi_Res = conf_.getParameter<edm::ParameterSet>("TH1Phi_Res");
   edm::ParameterSet psBend_Res = conf_.getParameter<edm::ParameterSet>("TH1Bend_Res");
   std::string HistoName;
@@ -500,9 +509,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta z Barrel PS modules";
   z_res_isPS_barrel = iBooker.book1D(HistoName,
                                      HistoName,
-                                     psZ_Res.getParameter<int32_t>("Nbinsx"),
-                                     psZ_Res.getParameter<double>("xmin"),
-                                     psZ_Res.getParameter<double>("xmax"));
+                                     ps_PS_Res.getParameter<int32_t>("Nbinsx"),
+                                     ps_PS_Res.getParameter<double>("xmin"),
+                                     ps_PS_Res.getParameter<double>("xmax"));
   z_res_isPS_barrel->setAxisTitle("tp_z - stub_z", 1);
   z_res_isPS_barrel->setAxisTitle("events ", 2);
 
@@ -510,9 +519,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta z Barrel 2S modules";
   z_res_is2S_barrel = iBooker.book1D(HistoName,
                                      HistoName,
-                                     psZ_Res.getParameter<int32_t>("Nbinsx"),
-                                     psZ_Res.getParameter<double>("xmin"),
-                                     psZ_Res.getParameter<double>("xmax"));
+                                     ps_2S_Res.getParameter<int32_t>("Nbinsx"),
+                                     ps_2S_Res.getParameter<double>("xmin"),
+                                     ps_2S_Res.getParameter<double>("xmax"));
   z_res_is2S_barrel->setAxisTitle("tp_z - stub_z [cm]", 1);
   z_res_is2S_barrel->setAxisTitle("events ", 2);
 
@@ -520,9 +529,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta r FW Endcap PS modules";
   r_res_isPS_fw_endcap = iBooker.book1D(HistoName,
                                         HistoName,
-                                        psR_Res.getParameter<int32_t>("Nbinsx"),
-                                        psR_Res.getParameter<double>("xmin"),
-                                        psR_Res.getParameter<double>("xmax"));
+                                        ps_PS_Res.getParameter<int32_t>("Nbinsx"),
+                                        ps_PS_Res.getParameter<double>("xmin"),
+                                        ps_PS_Res.getParameter<double>("xmax"));
   r_res_isPS_fw_endcap->setAxisTitle("tp_r - stub_r [cm]", 1);
   r_res_isPS_fw_endcap->setAxisTitle("events ", 2);
 
@@ -530,9 +539,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta r FW Endcap 2S modules";
   r_res_is2S_fw_endcap = iBooker.book1D(HistoName,
                                         HistoName,
-                                        psR_Res.getParameter<int32_t>("Nbinsx"),
-                                        psR_Res.getParameter<double>("xmin"),
-                                        psR_Res.getParameter<double>("xmax"));
+                                        ps_2S_Res.getParameter<int32_t>("Nbinsx"),
+                                        ps_2S_Res.getParameter<double>("xmin"),
+                                        ps_2S_Res.getParameter<double>("xmax"));
   r_res_is2S_fw_endcap->setAxisTitle("tp_r - stub_r [cm]", 1);
   r_res_is2S_fw_endcap->setAxisTitle("events ", 2);
 
@@ -540,9 +549,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta r BW Endcap PS modules";
   r_res_isPS_bw_endcap = iBooker.book1D(HistoName,
                                         HistoName,
-                                        psR_Res.getParameter<int32_t>("Nbinsx"),
-                                        psR_Res.getParameter<double>("xmin"),
-                                        psR_Res.getParameter<double>("xmax"));
+                                        ps_PS_Res.getParameter<int32_t>("Nbinsx"),
+                                        ps_PS_Res.getParameter<double>("xmin"),
+                                        ps_PS_Res.getParameter<double>("xmax"));
   r_res_isPS_bw_endcap->setAxisTitle("tp_r - stub_r [cm]", 1);
   r_res_isPS_bw_endcap->setAxisTitle("events ", 2);
 
@@ -550,9 +559,9 @@ void Phase2OTValidateTTStub::bookHistograms(DQMStore::IBooker& iBooker,
   HistoName = "#Delta r BW Endcap 2S modules";
   r_res_is2S_bw_endcap = iBooker.book1D(HistoName,
                                         HistoName,
-                                        psR_Res.getParameter<int32_t>("Nbinsx"),
-                                        psR_Res.getParameter<double>("xmin"),
-                                        psR_Res.getParameter<double>("xmax"));
+                                        ps_2S_Res.getParameter<int32_t>("Nbinsx"),
+                                        ps_2S_Res.getParameter<double>("xmin"),
+                                        ps_2S_Res.getParameter<double>("xmax"));
   r_res_is2S_bw_endcap->setAxisTitle("tp_r - stub_r [cm]", 1);
   r_res_is2S_bw_endcap->setAxisTitle("events ", 2);
 
@@ -680,14 +689,14 @@ void Phase2OTValidateTTStub::fillDescriptions(edm::ConfigurationDescriptions& de
     psd0.add<int>("Nbinsx", 99);
     psd0.add<double>("xmax", 5.5);
     psd0.add<double>("xmin", -5.5);
-    desc.add<edm::ParameterSetDescription>("TH1z_Res", psd0);
+    desc.add<edm::ParameterSetDescription>("TH1_2S_Res", psd0);
   }
   {
     edm::ParameterSetDescription psd0;
     psd0.add<int>("Nbinsx", 99);
-    psd0.add<double>("xmax", 5.5);
-    psd0.add<double>("xmin", -5.5);
-    desc.add<edm::ParameterSetDescription>("TH1r_Res", psd0);
+    psd0.add<double>("xmax", 2.0);
+    psd0.add<double>("xmin", -2.0);
+    desc.add<edm::ParameterSetDescription>("TH1_PS_Res", psd0);
   }
   {
     edm::ParameterSetDescription psd0;
@@ -708,12 +717,15 @@ void Phase2OTValidateTTStub::fillDescriptions(edm::ConfigurationDescriptions& de
   desc.add<edm::InputTag>("TTStubs", edm::InputTag("TTStubsFromPhase2TrackerDigis", "StubAccepted"));
   desc.add<edm::InputTag>("trackingParticleToken", edm::InputTag("mix", "MergedTrackTruth"));
   desc.add<edm::InputTag>("MCTruthStubInputTag", edm::InputTag("TTStubAssociatorFromPixelDigis", "StubAccepted"));
+  desc.add<edm::InputTag>("MCTruthClusterInputTag",
+                          edm::InputTag("TTClusterAssociatorFromPixelDigis", "ClusterInclusive"));
   desc.add<int>("TP_minNStub", 4);
   desc.add<int>("TP_minNLayersStub", 4);
   desc.add<double>("TP_minPt", 2.0);
   desc.add<double>("TP_maxEta", 2.4);
   desc.add<double>("TP_maxVtxZ", 15.0);
   desc.add<double>("TP_maxD0", 1.0);
+  desc.add<double>("TP_maxDxy", 1.0);
   descriptions.add("Phase2OTValidateTTStub", desc);
   // or use the following to generate the label from the module's C++ type
   //descriptions.addWithDefaultLabel(desc);
